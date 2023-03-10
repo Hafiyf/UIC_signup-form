dcaebe84fc9dd95b13145955f5340bb8140f8207
joa4k <jamshid.oa4k@gmail.com>
2023-03-10 12:52:43 +0500

    btns have called on js file

---
+++ JavaScript/main.js
@@ -0,0 +1,7 @@
+const loginBtn = document.querySelector('#login__btn')
+const regBtn = document.querySelector('#register-btn')
+const registerTexts = document.querySelector('#register')
+
+addEventListener.loginBtn('onclick', )
+
+
