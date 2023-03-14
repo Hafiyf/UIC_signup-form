b0e8bf21a81905f3714e6a5ee5e5264032f84c9a
Jamshid <jamshid.oa4k@gmail.com>
2023-03-14 17:54:21 +0500

    have to do some changes on inputs

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,19 +5,21 @@ const signinTexts = document.querySelector('#signinTexts')
 
 
 function loginClick(){
-    document.querySelector('#whiteBox').style.left = "350px";
-    document.querySelector('#register').classList.remove('disabled')
+    document.querySelector('#whiteBox').style.left = "400px";
+    document.querySelector('#register').classList.remove('disable')
     document.querySelector('#signinTexts').classList.add('disabled')
     document.querySelector('#white__reg').classList.add('disable')
     document.querySelector('#whiteLogin').classList.remove('disable')
+    document.querySelector('#signinTexts').style.marginLeft = '0px';
 
 }
 
 function signupClick(){
     document.querySelector('#whiteBox').style.left = "";
-    document.querySelector('#register').classList.add('disabled')
+    document.querySelector('#register').classList.add('disable')
     document.querySelector('#signinTexts').classList.remove('disabled')
     document.querySelector('#white__reg').classList.remove('disable')
     document.querySelector('#whiteLogin').classList.add('disable')
+    document.querySelector('#signinTexts').style.marginLeft = '';
 
 }
