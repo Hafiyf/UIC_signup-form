714c583f6f3b79ebe9a32f12c695b4a0576822fb
Jamshid <jamshid.oa4k@gmail.com>
2023-03-14 15:17:59 +0500

    JS ready. only left some changes on white login

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,9 +3,21 @@ const regBtn = document.querySelector('#register-btn')
 const registerTexts = document.querySelector('#register')
 const signinTexts = document.querySelector('#signinTexts')
 
-signinTexts.addEventListener('click', )
 
 function loginClick(){
-    
+    document.querySelector('#whiteBox').style.left = "47rem";
+    document.querySelector('#register').classList.remove('disabled')
+    document.querySelector('#signinTexts').classList.add('disabled')
+    document.querySelector('#white__reg').classList.add('disable')
+    document.querySelector('#whiteLogin').classList.remove('disable')
+
 }
 
+function signupClick(){
+    document.querySelector('#whiteBox').style.left = "";
+    document.querySelector('#register').classList.add('disabled')
+    document.querySelector('#signinTexts').classList.remove('disabled')
+    document.querySelector('#white__reg').classList.remove('disable')
+    document.querySelector('#whiteLogin').classList.add('disable')
+
+}
--- JavaScript/transition.js
+++ JavaScript/transition.js
@@ -12,14 +12,26 @@ const regBtn = document.querySelector('#register-btn')
 const registerTexts = document.querySelector('#register')
 const signinTexts = document.querySelector('#signinTexts')
 
-document.getElementById("login-btn").onclick = leftClick;
+let signUpButton = document.getElementById("signup");
+let loginButton = document.getElementById("login");
+let forms = document.getElementById("forms");
+let signUpForm = document.getElementById("signUpForm");
+let loginForm = document.getElementById("loginForm");
 
-document.getElementById("register-btn").onclick = rightClick;
-
-function leftClick() {
-  document.getElementById("whiteBox").className = "leftbox";
-}
-
-function rightClick() {
-  document.getElementById("whiteBox").className = "rightbox";
+function removeButton(id) {
+  if (id == "signup") {
+    signUpButton.style.opacity = "0";
+    loginButton.style.opacaity = "1";
+    forms.classList.remove('toRight')
+    forms.classList.add('toLeft')
+    loginForm.style.display = 'none'
+    signUpForm.style.display = 'block'
+  } else {
+    loginButton.style.opacity = "0";
+    signUpButton.style.opacity = "1";
+    forms.classList.remove('toLeft')
+    forms.classList.add('toRight')
+    signUpForm.style.display = 'none'
+    loginForm.style.display = 'block'
+  }
 }
